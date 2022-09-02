a0483f8b88c3571b52f4cd0c9961fdff3cd34a17
Daniel Macharia <danielhuios@gmail.com>
2022-09-02 13:11:34 +0300

    added event listener 'click' and fetch function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,7 @@
+const API_KEY = "7d149566af8dd84bd3a1e75d071091be";
+const URL =
+	"https://api.themoviedb.org/3/search/movie?api_key=7d149566af8dd84bd3a1e75d071091be&query=thor";
+
 //DOM elements
 const submitElement = document.getElementById("searchTab");
 const inputElement = document.getElementById("inputValue");
@@ -7,8 +11,17 @@ const movieContainer = document.getElementById("movie-container");
 submitElement.addEventListener("click", (e) => {
 	e.preventDefault();
 	const value = inputElement.value;
-    console.log(value);
+
+	fetch(URL)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+
+	console.log(value);
 
 	inputElement.value = ""; // reset
 });
-
